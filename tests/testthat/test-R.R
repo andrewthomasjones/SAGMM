071f5dd4dd65bd369dead8c4ce3b200ad51dcb94
Andrew Jones <andrewjones@Andrews-MacBook-Pro.local>
2019-03-02 17:30:31 +1000

    Added testing

---
+++ tests/testthat/test-R.R
@@ -0,0 +1,57 @@
+context("Check native R Functions")
+library(SAGMM)
+
+test_that("Test gainFactors", {
+    
+    number<-100
+    burnin<-100
+    result<-gainFactors(number, burnin)
+    
+    #types correct
+    expect_is(result, "numeric")
+    #size
+    expect_equal(length(result), x)
+    #no missing
+    expect_identical(result, na.omit(result))
+    
+})
+
+test_that("Test generateSimData", {
+    
+    result<-generateSimData(Groups=10, Dimensions=10, Number=10^2)
+   
+    #types correct
+    expect_is(result, "list")
+    expect_is(result[[1]], "matrix")
+    expect_is(result[[2]], "integer")
+    expect_is(result[[3]], "MixSim")
+    
+    #no missing
+    expect_identical(result, na.omit(result))
+    
+})
+
+test_that("Test SAGMMFit", {
+    sims<-generateSimData(Groups=10, Dimensions=10, Number=10^4)
+    result<-SAGMMFit(sims$X, sims$Y, sims$MS)
+    
+    # #types correct
+    # expect_is(result, "list")
+    # expect_is(result[[1]], "numeric")
+    # expect_is(result[[2]], "numeric")
+    # expect_is(result[[3]], "numeric")
+    # expect_is(result[[4]], "numeric")
+    # expect_is(result[[5]], "numeric")
+    # expect_is(result[[6]], "matrix")
+    # 
+    # #dims
+    # expect_equal(result[[2]], N)
+    # expect_equal(result[[3]], DIM)
+    # expect_equal(length(result[[1]]), DIM+1)
+    # expect_equal(dim(result[[4]]), c(N,DIM+1))
+    
+    #no missing
+    expect_identical(result, na.omit(result))
+    
+})
+
--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -1,8 +1,75 @@
 context("Check exported C++ Functions")
+library(SAGMM)
 
+test_that("mahalanobis_HD", {
+    
+    size<-sample(2:10,1)
+    x<-runif(size)
+    y<-runif(size)
+    A <- matrix(runif(size^2)*2-1, ncol=size) 
+    Sigma <- t(A) %*% A
+    result<-mahalanobis_HD(x,y,Sigma)
+    
+    #types correct
+    expect_is(result, "numeric")
+   
+    #no missing
+    expect_identical(result, na.omit(result))
+    
+})
 
-mahalanobis_HD 
+test_that("norm_HD", {
+    
+    size<-sample(2:10,1)
+    x<-runif(size)
+    y<-runif(size)
+    A <- matrix(runif(size^2)*2-1, ncol=size) 
+    Sigma <- t(A) %*% A
+    result<-norm_HD(x,y,Sigma)
+    
+    #types correct
+    expect_is(result, "numeric")
+    
+    #no missing
+    expect_identical(result, na.omit(result))
+    
+})
 
-norm_HD 
+test_that("main_loop", {
+    #set up for test
+   
+    
+    result<-main_loop(Number, Groups, PISTAR_O, MU_O, 
+                      LAMBDA_O, GAMMA, X,
+                       Dimensions, SIGMA)
+    
+    # Rcpp::List retList = Rcpp::List::create(
+    #     Rcpp::Named("PI")= export_vec(PIvec),
+    #     Rcpp::Named("MU")= (MU),
+    #     Rcpp::Named("LAMBDA")= export_vec(LAMBDA),
+    #     Rcpp::Named("Comps")= export_vec(Comps),
+    #     Rcpp::Named("LogLike")= LogLike,
+    #     Rcpp::Named("Tau")= TauMAT
+    #     
+    # );
+    
+    #types correct
+    expect_is(result, "list")
+    expect_is(result[[1]], "numeric")
+    expect_is(result[[2]], "numeric")
+    expect_is(result[[3]], "numeric")
+    expect_is(result[[4]], "numeric")
+    expect_is(result[[5]], "numeric")
+    expect_is(result[[6]], "matrix")
+    
+    #dims
+    expect_equal(result[[2]], N)
+    expect_equal(result[[3]], DIM)
+    expect_equal(length(result[[1]]), DIM+1)
+    expect_equal(dim(result[[4]]), c(N,DIM+1))
+    
+    #no missing
+    expect_identical(result, na.omit(result))
+    
+})
 
-main_loop
