e4f831eae424204a47594a01bafe88b46cf822a4
Andrew Jones <andrewthomasjones@gmail.com>
2019-05-06 19:39:37 +1000

    add more tests

--- tests/testthat/test-R.R
+++ tests/testthat/test-R.R
@@ -33,28 +33,58 @@ test_that("Test generateSimData", {
 
 test_that("Test SAGMMFit", {
     sims<-generateSimData(Groups=10, Dimensions=10, Number=10^3)
-    result<-SAGMMFit(sims$X, sims$Y, sims$MS)
+    result<-SAGMMFit(sims$X, sims$Y)
     
     # #types correct
     expect_is(result, "list")
     expect_is(result[[1]], "integer")
     expect_is(result[[2]], "logical")
     expect_is(result[[3]], "numeric")
-    expect_is(result[[4]], "array")
+    expect_is(result[[4]], "numeric")
     expect_is(result[[5]], "numeric")
-    expect_is(result[[6]], "numeric")
-    expect_is(result[[7]], "kmeans")
+    expect_is(result[[6]], "kmeans")
+    expect_is(result[[7]], "numeric")
     expect_is(result[[8]], "numeric")
-    expect_is(result[[9]], "numeric")
-    expect_is(result[[10]], "MixSim")
-    expect_is(result[[11]], "matrix")
-    expect_is(result[[12]], "list")
+    expect_is(result[[9]], "matrix")
+    expect_is(result[[10]], "list")
     
     #dims
-    expect_equal(length(result), 12)
+    expect_equal(length(result), 10)
    
     #no missing
     expect_identical(result, na.omit(result))
     
+    result<-SAGMMFit(sims$X, Groups=5)
+    # #types correct
+    expect_is(result, "list")
+    expect_is(result[[1]], "integer")
+    expect_is(result[[2]], "logical")
+    expect_is(result[[3]], "numeric")
+    expect_is(result[[4]], "logical")
+    expect_is(result[[5]], "logical")
+    expect_is(result[[6]], "kmeans")
+    expect_is(result[[7]], "logical")
+    expect_is(result[[8]], "numeric")
+    expect_is(result[[9]], "matrix")
+    expect_is(result[[10]], "list")
+    
+    #dims
+    expect_equal(length(result), 10)
+    
 })
 
+test_that("Test SAGMMFit Plots", {
+    sims<-generateSimData(Groups=5, Dimensions=2, Number=10^2)
+    result1<-SAGMMFit(sims$X, sims$Y, plot=TRUE)
+    result2<-SAGMMFit(sims$X, Groups=4, plot=TRUE)
+    
+    #dims
+    expect_is(result1, "list")
+    expect_equal(length(result1), 10)
+    expect_is(result2, "list")
+    expect_equal(length(result2), 10)
+    
+    expect_is(result1[[2]], "NULL")
+    expect_is(result2[[2]], "NULL")
+
+})
